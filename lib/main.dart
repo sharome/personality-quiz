82eb6f26091a2488941a8e7e23408db633565898
darkspace-coder <77049616+darkspace-coder@users.noreply.github.com>
2021-01-07 12:04:28 +1100

    bakend of question change commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,18 @@ import 'package:flutter/material.dart';
 //void main() {
 //runApp(MyApp());
 //}
-void main() => runApp (MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    void answerQuestion(){
-      print ('answerChosen');
-
-    }
-
     var questions = [
       'What\'s your favourite colour?',
       'What\'s your favourite animal?'
@@ -24,18 +26,20 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The question'),
+            Text(
+              questions[questionIndex],
+            ),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: answerQuestion,
+              onPressed: () => print('Answer 2 chosen'),
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: answerQuestion,
+              onPressed: () => print('Answer 3 chosen'),
             ),
           ],
         ),

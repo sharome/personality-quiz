bc77033adae549b8642fadaf4203e309e7cd8bed
darkspace-coder <77049616+darkspace-coder@users.noreply.github.com>
2021-01-07 11:14:57 +1100

    fix app not starting issue

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 //void main() {
 //runApp(MyApp());
 //}
-void main() => (MyApp());
+void main() => runApp (MyApp());
 
 class MyApp extends StatelessWidget {
   @override

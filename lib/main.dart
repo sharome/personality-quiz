3064d43bb94b4df16afeaec3f1dd770d567ba1c3
darkspace-coder <77049616+darkspace-coder@users.noreply.github.com>
2021-01-07 16:55:20 +1100

    question.dart added, Text6 centered

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import'./question.dart';
+
 //void main() {
 //runApp(MyApp());
 //}
@@ -15,13 +17,13 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  var questionIndex = 0;
+  var _questionIndex = 0;
 
   void answerQuestion() {
     setState((){
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -37,8 +39,8 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(
-              questions[questionIndex],
+            Question(
+              questions[_questionIndex],
             ),
             RaisedButton(
               child: Text('Answer 1'),
@@ -58,4 +60,4 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-//gitty
+
